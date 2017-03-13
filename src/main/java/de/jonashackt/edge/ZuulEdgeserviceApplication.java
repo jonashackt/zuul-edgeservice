c68516c0efc9fc93f06d72fbce6ef4fee7a4a02b
Jonas Hecht <jonas.hecht@codecentric.de>
2017-03-13 10:23:57 +0100

    connecting to Eureka

--- src/main/java/de/jonashackt/edge/ZuulEdgeserviceApplication.java
+++ src/main/java/de/jonashackt/edge/ZuulEdgeserviceApplication.java
@@ -1,13 +1,23 @@
 package de.jonashackt.edge;
 
 import de.jonashackt.edge.filters.pre.LoggingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class ZuulEdgeserviceApplication {
 
 	public static void main(String[] args) {
@@ -17,5 +27,18 @@ public class ZuulEdgeserviceApplication {
 	@Bean
 	public LoggingFilter loggingFilter() {
 		return new LoggingFilter();
+	}
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(
+			@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
 	}
 }
