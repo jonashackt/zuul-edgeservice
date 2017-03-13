98140d208c7b58f4940d36b82b3be3a723ba4a54
Jonas Hecht <jonas.hecht@codecentric.de>
2017-03-13 15:16:42 +0100

    Move together seperate projects

--- src/main/java/de/jonashackt/edge/ZuulEdgeserviceApplication.java
+++ src/main/java/de/jonashackt/edge/ZuulEdgeserviceApplication.java
@@ -1,23 +1,13 @@
 package de.jonashackt.edge;
 
 import de.jonashackt.edge.filters.pre.LoggingFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 public class ZuulEdgeserviceApplication {
 
 	public static void main(String[] args) {
@@ -30,15 +20,3 @@ public class ZuulEdgeserviceApplication {
 	}
 }
 
-@RestController
-class ServiceInstanceRestController {
-
-	@Autowired
-	private DiscoveryClient discoveryClient;
-
-	@RequestMapping("/service-instances/{applicationName}")
-	public List<ServiceInstance> serviceInstancesByApplicationName(
-			@PathVariable String applicationName) {
-		return this.discoveryClient.getInstances(applicationName);
-	}
-}
